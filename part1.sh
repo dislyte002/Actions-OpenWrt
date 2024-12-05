0d918ad3e566b4852aae5cabe9b843a8574d021c
dislyte002 <141597110+dislyte002@users.noreply.github.com>
2024-12-05 10:38:41 +0800

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -23,7 +23,7 @@
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-jd-dailybonus
-sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #gsed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # 删除旧主题
