e53b81a3ffcaeee961e54c04855430e60519931e
dislyte002 <satall@live.cn>
2024-12-27 16:44:27 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,10 +23,17 @@
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-jd-dailybonus
+<<<<<<< HEAD
 #sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
 # sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev' feeds.conf.default
+=======
+sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev' feeds.conf.default
+>>>>>>> parent of c2416bd (Update diy-part1.sh)
 
 # 删除旧主题
 rm -rf feeds/kenzo/luci-theme-argon
