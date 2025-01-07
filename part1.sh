5ca50a5317e454649f1168f2a3534f7d7029819b
dislyte002 <141597110+dislyte002@users.noreply.github.com>
2025-01-07 12:38:11 +0800

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -16,6 +16,12 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
+#sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 # 下载新主题
 # rm -rf ./package/lean/autocore
 # git clone -b lede https://github.com/supsupsuperstar/autocore.git package/lean/autocore
@@ -23,8 +29,7 @@
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-jd-dailybonus
-sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 
 # 删除旧主题
 rm -rf feeds/kenzo/luci-theme-argon
